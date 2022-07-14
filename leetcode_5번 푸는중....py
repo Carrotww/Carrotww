f49d745afd8aa0a7edc161ba0a7808bbd25ad97d
Carrotww <104682050+Carrotww@users.noreply.github.com>
2022-07-15 01:51:52 +0900

    Create leetcode_5번 푸는중....py

---
+++ leetcode_5번 푸는중....py
@@ -0,0 +1,43 @@
+from collections import deque
+import copy
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        s = deque(s)
+        ss = copy.deepcopy(s)
+        result_1 = list()
+        while s:
+            if list(s) == list(s)[::-1]:
+                result_1.append(s)
+                break
+            elif not s:
+                break
+            s.pop()
+            if list(s) == list(s)[::-1]:
+                result_1.append(s)
+                break
+            elif not s:
+                break
+            s.popleft()
+        while ss:
+            if list(ss) == list(ss)[::-1]:
+                result_1.append(ss)
+                break
+            elif not ss:
+                break
+            s.popleft()
+            if list(ss) == list(ss)[::-1] and not ss:
+                result_1.append(ss)
+                break
+            elif not ss:
+                break
+            s.pop()
+        if not result_1:
+            return ''.join(s)
+        return result_1
+
+a = Solution()
+
+print(a.longestPalindrome("babad"))
+print(a.longestPalindrome("ccc"))
+print(a.longestPalindrome("abb"))
