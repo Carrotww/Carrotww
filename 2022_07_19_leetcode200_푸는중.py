16b40d62301b035c2a4492f2135efe4ab33c0bda
Carrotww <104682050+Carrotww@users.noreply.github.com>
2022-07-19 21:54:06 +0900

    Update 2022_07_19_leetcode200_푸는중.py

--- 2022_07_19_leetcode200_푸는중.py
+++ 2022_07_19_leetcode200_푸는중.py
@@ -4,15 +4,31 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid:
             return 0
-        row, col = len(grid[0]) - 1, len(grid) - 1
+        row, col = len(grid[0]), len(grid)
+        check_list = []
         cnt = 0
-        def fun1(x, y):
-            while True:
 
+        def fun1(c, r):
+            i = 0
+            while grid[c + i][r] == "1" and [c + i, r] not in check_list:
+                check_list.append([c + i, r])
+                i += 1
+                j = 0
+                if c + i < len(grid):
+                    break
+                while grid[c][r + j] == "1" and [c, r + j] not in check_list:
+                    check_list.append([c, r + j])
+                    j += 1
+                    if r + j < len(grid[0]):
+                        break
+        for x in range(col):
+            for y in range(row):
+                ch_len = len(check_list)
+                fun1(x, y)
+                if ch_len != len(check_list):
+                    cnt += 1
 
-        fun1(0, 0)
-
-        return grid
+        return cnt
 
 a = Solution()
 
