35e44c053a4d78f6762ecf26acad03afa42fed17
Carrotww <104682050+Carrotww@users.noreply.github.com>
2022-07-28 23:05:41 +0900

    Update 2022_07_24_programmers_추석 트래픽 푸는 중.py

--- 2022_07_24_programmers_추석 트래픽 푸는 중.py
+++ 2022_07_24_programmers_추석 트래픽 푸는 중.py
@@ -1,21 +1,29 @@
-import collections
 from typing import List
 
 def solution(lines):
-    result = ''
     log_line = [[a, b, c] for a, b, c in [x.split() for x in lines]]
+    end_time = [total_time(x) for x in log_line]
+    start_time = [makestart_time(x) for x in log_line]
+    result = 0
 
-    def total_time(line: List) -> int:
-        if not line:
-            return 0
-        log_Hour = int(line[1].split(":")[0])
-        log_min = int(line[1].split(":")[1])
-        log_sec = int(line[1].split(":")[2].split(".")[0])
-        log_msec = int(line[1].split(":")[2].split(".")[1])
+    for i in range(len(lines)):
+        cnt = 0
+        cur_end_time = end_time[i]
 
-        return log_msec + (log_sec * 100) + (log_min * 6000) + (log_Hour * 360000)
+        for j in range(i, len(lines)):
+            if cur_end_time > start_time[j] - 1000:
+                cnt += 1
+        result = max(result, cnt)
+    return result
 
-    return total_time(log_line[0])
+def total_time(line: List) -> float:
+    log_Hour = int(line[1].split(":")[0])
+    log_min = int(line[1].split(":")[1])
+    log_sec = float(line[1].split(":")[2])
+    return int((log_sec + (log_min * 60) + (log_Hour * 3600)) * 1000)
+
+def makestart_time(line: List) -> int:
+    return int(total_time(line) - float(line[2].split('s')[0]) * 1000) + 1
 
 li = [
 "2016-09-15 01:00:04.001 2.0s",
