b816c6faeb9eeaa11cda98e0b36335ddf5a40d92
Carrotww <104682050+Carrotww@users.noreply.github.com>
2022-09-07 11:22:42 +0900

    commit

---
+++ None_Tech/22_09/2022_09_07_programmers_level 1_같은 숫자는 싫어_hyeong.py
@@ -0,0 +1,9 @@
+def solution(arr):
+    result = [arr[0]]
+    for i in range(1, len(arr)):
+        if result[-1] == arr[i]:
+            continue
+        else:
+            result.append(arr[i])
+    
+    return result
