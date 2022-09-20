6e598849c1121305b093a44a6f6ee9e0e59a78d9
Carrot_hyeong <104682050+Carrotww@users.noreply.github.com>
2022-09-20 11:39:46 +0900

    commit

--- Dynamic Programming/2022_09_15_programmers_N으로 표현_Level3_hyeong.py
+++ Dynamic Programming/2022_09_15_programmers_N으로 표현_Level3_hyeong.py
@@ -9,25 +9,22 @@ def solution(n, number):
                 temp.add(i + j)
                 temp.add(i * j)
                 temp.add(j - i)
-                temp.add(i - j)
+                # temp.add(i - j)
                 if j != 0: temp.add(i // j)
-                if i != 0: temp.add(j // i)
+                # if i != 0: temp.add(j // i)
         return temp # 입력받은 두 개의 set 을 모두 계산하여서 temp 에 저장
     
     temp = defaultdict(set)
     
-    for i in range(1, 10):
-        if i == 1:
-            temp[i].add(n)
-        else:
-            str_append = int(str(n) * i)
-            temp[i].add(str_append)
-            for x in range(1, i):
-                for y in range(1, i):
-                    if x + y == i:
-                        temp_result = fun1(temp[x], temp[y])
-                        for re in temp_result:
-                            temp[i].add(re)
+    for i in range(1, 9):
+        str_append = int(str(n) * i)
+        temp[i].add(str_append)
+        for x in range(1, i):
+            for y in range(1, i):
+                if x + y == i:
+                    temp_result = fun1(temp[x], temp[y])
+                    for re in temp_result:
+                        temp[i].add(re)
         if number in temp[i]:
             return i
     return -1
