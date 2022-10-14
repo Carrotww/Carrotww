17645102c5e345a6ff991d86f0dee7f7908d85be
Carrotww <104682050+Carrotww@users.noreply.github.com>
2022-10-15 02:24:30 +0900

    게임맵 최단 거리 풀이, DFS 시도하였지만 효율성에서 통과 하나도 안됨, BFS로 시도하여 풀이 완료 나중에 복습 꼭 할것

--- BFS,DFS/2022_10_14_programmers_게임맵최단거리_hyeong.py
+++ BFS,DFS/2022_10_14_programmers_게임맵최단거리_hyeong.py
@@ -2,37 +2,57 @@
 # 푸는 중
 
 def solution(maps):
-    result = []
-    row_last = len(maps[0]) - 1
-    col_last = len(maps) - 1
-    
-    def dfs(maps, row, col, cnt):
-        if row == row_last and col == col_last:
-            result.append(cnt)
-            return
-        if row > row_last or col > col_last:
-            return
-        if row + 1 <= row_last and maps[col][row + 1] == 1:
-            temp = maps[:]
-            temp[col][row + 1] = 0
-            dfs(temp, row + 1, col, cnt + 1)
-        if row - 1 >= 0 and maps[col][row - 1] == 1:
-            temp = maps[:]
-            temp[col][row - 1] = 0
-            dfs(temp, row - 1, col, cnt + 1)
-        if col + 1 <= col_last and maps[col + 1][row] == 1:
-            temp = maps[:]
-            temp[col + 1][row] = 0
-            dfs(temp, row, col + 1, cnt + 1)
-        if col - 1 >= 0 and maps[col - 1][row] == 1:
-            temp = maps[:]
-            temp[col - 1][row] = 0
-            dfs(temp, row, col - 1, cnt + 1)
-    
-    dfs(maps, 0, 0, 1)
+    from collections import deque
+    total_row, total_col = len(maps) - 1, len(maps[0]) - 1
+    cnt = 0
+    queue = deque()
+    queue.append([0, 0])
+    visited = [[0]*(total_col + 1) for _ in range(total_row + 1)]
+    visited[0][0] = 1
+    # 동서남북
+    dr, dc = [1, -1, 0, 0], [0, 0, -1, 1]
 
-    if result:
-        return min(result)
+    while queue:
+        current_r, current_c = queue.popleft()
+        if current_r == total_row and current_c == total_col:
+            return visited[current_r][current_c]
+
+        for i in range(4):
+            next_r, next_c = dr[i] + current_r, dc[i] + current_c
+            if 0 <= next_r <= total_row and 0 <= next_c <= total_col:
+                if maps[next_r][next_c] == 1 and not visited[next_r][next_c]:
+                    visited[next_r][next_c] = visited[current_r][current_c] + 1
+                    queue.append((next_r, next_c))
     return -1
 
-print(solution([[1,0], [1,0], [1,0],[1,1]]))
+"""
+# 기존 DFS 풀이 근데 이제 DP를 곁들인.
+from collections import deque
+
+def solution(maps):
+    result = 0
+    dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+    q = deque([0, 0])
+
+    visited = [[0] * len(maps[0]) for _ in range(len(maps))]
+    visited[0][0] = 1
+    maps[0][0] = 0
+    
+    while q:
+        x, y = q.popleft()
+        for k in range(4):
+            nx, ny = x + dx[k], y + dy[k]
+            if 0 <= nx <= len(maps) and 0 <= ny < len(maps[0]):
+                if maps[nx][ny] == 1 and not visited[nx][ny]:
+                    visited[nx][ny] = visited[x][y] + 1
+                    q.append((nx, ny))
+    
+    result = -1
+    if visited:
+        return visited
+    return result
+"""
+
+print(solution([[1,0], [1,0], [1,0], [1,1]]))
+print(solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]))
+print(solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]]))
