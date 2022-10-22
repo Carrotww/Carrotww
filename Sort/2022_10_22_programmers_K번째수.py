a3e05d185206346405688d403c7665bcbbfe8897
Carrotww <rjgjfl90@naver.com>
2022-10-23 00:26:15 +0900

    programmers k번째 수 done

--- Sort/2022_10_22_programmers_K번째수.py
+++ Sort/2022_10_22_programmers_K번째수.py
@@ -1,7 +1,13 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/42748
 
 def solution(array, commands):
-    answer = []
-    return answer
+    result = []
+
+    for com in commands:
+        temp = array[com[0]-1:com[1]]
+        temp.sort()
+        result.append(temp[com[2]-1])
+        
+    return result
 
 print(solution([1, 5, 2, 6, 3, 7, 4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]]))
