e5c461767aa7876088c28ee3bdc1eff46674aa75
Carrotww <rjgjfl90@naver.com>
2022-10-22 23:57:23 +0900

    k 번째 수 푸는 중

--- Sort/2022_10_22_programmers_K번째수.py
+++ Sort/2022_10_22_programmers_K번째수.py
@@ -1,1 +1,7 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/42748
+
+def solution(array, commands):
+    answer = []
+    return answer
+
+print(solution([1, 5, 2, 6, 3, 7, 4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]]))
