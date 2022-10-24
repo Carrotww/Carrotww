da4f9b6587267eb5f9680049b7c123cd57055e2d
Carrotww <104682050+Carrotww@users.noreply.github.com>
2022-10-24 20:46:35 +0900

    H-index 추가 풀이

--- Sort/2022_10_22_programmers_H-Index.py
+++ Sort/2022_10_22_programmers_H-Index.py
@@ -1,9 +1,19 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/42747
 
+"""
 def solution(citations):
     citations.sort()
     
     for i in range(len(citations)):
         if citations[i] >= len(citations) - i:
             return len(citations) - i
+    return 0
+"""
+
+def solution(citations):
+    citations.sort()
+    
+    for num, val in enumerate(citations):
+        if val >= len(citations) - num:
+            return len(citations) - num
     return 0
