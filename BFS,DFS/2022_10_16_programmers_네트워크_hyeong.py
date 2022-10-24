68e3b40456749504cf48b77339e657a186a8d9db
Carrotww <104682050+Carrotww@users.noreply.github.com>
2022-10-24 16:15:35 +0900

    commit

--- BFS,DFS/2022_10_16_programmers_네트워크_hyeong.py
+++ BFS,DFS/2022_10_16_programmers_네트워크_hyeong.py
@@ -1,1 +1,9 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/43162
+
+def solution(n, computers):
+
+    
+    return
+
+print(solution(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]]))
+print(solution(3, [[1, 1, 0], [1, 1, 1], [0, 1, 1]]))
