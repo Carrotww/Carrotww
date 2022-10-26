49b4d43df62a5779e3e952f40044e41b4bef1090
Carrotww <104682050+Carrotww@users.noreply.github.com>
2022-10-26 23:01:21 +0900

    bfs, dfs programmers 네트워크 풀이

--- BFS,DFS/2022_10_16_programmers_네트워크_hyeong.py
+++ BFS,DFS/2022_10_16_programmers_네트워크_hyeong.py
@@ -1,9 +1,20 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/43162
 
 def solution(n, computers):
+    cnt, visited = 0, []
+
+    def dfs(index):
+        visited.append(index)
+        for i in range(n):
+            if computers[index][i] == 1 and i not in visited:
+                dfs(i)
     
+    for i in range(n):
+        if i not in visited:
+            dfs(i)
+            cnt += 1
     
-    return
+    return cnt
 
 print(solution(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]]))
 print(solution(3, [[1, 1, 0], [1, 1, 1], [0, 1, 1]]))
