19c5d765309119736a2e28c42d5d5fe85690314f
Carrotww <104682050+Carrotww@users.noreply.github.com>
2022-10-27 19:16:59 +0900

    carrot_Algorithm 으로 이사

--- None_Tech/22_09/2022_09_07_programmers_level 1_같은 숫자는 싫어_hyeong.py
+++
@@ -1,9 +0,0 @@
-def solution(arr):
-    result = [arr[0]]
-    for i in range(1, len(arr)):
-        if result[-1] == arr[i]:
-            continue
-        else:
-            result.append(arr[i])
-    
-    return result
